0e46ee6fed68ecd166ad27fa0494b2036f7ad356
ingvar75 <ingvar0208@gmail.com>
2021-08-19 17:17:10 +0300

    t_and_com new

--- l04/tags_and_comments.php
+++ l04/tags_and_comments.php
@@ -1,1 +1,28 @@
+test message
 <?php
+
+// Чувак, бери деньги вперед, заказчик мудак
+# Чувак, бери деньги вперед, заказчик мудак
+
+/*
+ * you can write
+ * multi-line comments
+ */
+
+echo 123 + 44; // counting bonuses
+echo 123 + 44; # counting bonuses
+
+/**
+ * @var string $test
+ */
+
+//var_dump($test);
+
+?>
+<h1>PHP EXAMPLE</h1>
+<p>User <?php echo 'Dmytro' ?> has $<?php echo 100 ?></p>
+<p>User <?= 'Dmytro' ?> has $<?= 100 ?></p>
+
+<?php
+
+echo 44444, '<br>', 5555;
