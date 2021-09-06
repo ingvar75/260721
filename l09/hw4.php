c26ff1b395ca7de31504b4203e225831bc80389d
ingvar75 <ingvar0208@gmail.com>
2021-09-06 16:51:25 +0300

    hw4 new

--- l09/hw4.php
+++ l09/hw4.php
@@ -1,1 +1,43 @@
 <?php
+$counter = 0;
+$array = [
+    'test' => 123123,
+    'qwerty' => 'qqqq',
+    'data' => [
+        1, 2, 3,
+    ],
+    'q' => [
+        'w' => [1, 2],
+        'r' => [1, 2],
+    ],
+];
+
+
+function countArray($array)
+{
+    global $counter;
+
+        $counter++;
+        $key = array_keys($array);
+        //var_dump($array);
+        foreach ($key as $item){
+            if (gettype($array[$item]) != 'array'){
+                echo $item.'=>'.$array[$item]."<br>";
+            }else {
+                echo $item . '=>' . countArray($array[$item]) . "<br>";
+                  }
+        }
+    return;
+}
+echo countArray($array), $counter;
+
+echo '<hr>';
+
+// HW
+// написать рекурсивную функцию для просчета количества элементов в массиве
+var_dump(count($array, true));
+var_dump(array_keys($array, true));
+// написать рекурсивную функцию для вывода массива на экран
+echo '<pre>';
+print_r($array);
+echo '</pre>';
