fea397438aa4fc85b0b8a6c339cbae4e7734da1e
ingvar75 <ingvar0208@gmail.com>
2021-11-26 11:40:14 +0200

    21shop add

--- l21-shop/app/views/layouts/main.php
+++ l21-shop/app/views/layouts/main.php
@@ -53,9 +53,9 @@ $cart = new Cart();
     <?= $this->getContent() ?>
 </div>
 <script
-        src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-        crossorigin="anonymous"></script>
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
 <script src="/public/js/bootstrap.bundle.js"></script>
 <script src="/public/js/buy.js"></script>
 <script>
