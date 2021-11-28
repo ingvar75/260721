5c17e11ce8e4985702460fb1469ec03bf9d27b55
ingvar75 <ingvar0208@gmail.com>
2021-11-28 19:30:18 +0200

    l28chat add

--- l28-chat/routes/web.php
+++ l28-chat/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\GuestController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\RoomsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(static function () {
+    Route::get('register', fn () => view('guest.register'));
+    Route::get('login', fn () => view('guest.login'))->name('login');
+
+    Route::post('register', [GuestController::class, 'register']);
+    Route::post('login', [GuestController::class, 'login']);
+});
+
+Route::middleware('auth')->group(static function () {
+    Route::get('/', [IndexController::class, 'index'])->name('home');
+    Route::get('profile/view', fn () => view('welcome'))->name('profile');
+    Route::post('rooms/create', [RoomsController::class, 'create'])->name('createRoom');
+    Route::get('logout', [UserController::class, 'logout'])->name('logout');
 });
