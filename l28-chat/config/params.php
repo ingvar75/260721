0234b71131b6843518565104c714f1e07e2e3767
ingvar75 <ingvar0208@gmail.com>
2021-11-29 08:38:03 +0200

    l28chat add

---
+++ l28-chat/config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'rooms' => [
+        'avatarsDir' => 'images/rooms',
+        'avatarUrl' => '/images/rooms',
+        'defaultChannelAvatar' => '/images/rooms/default.png',
+    ]
+];
