119a3fe432dbada2f6a5812770669b8660ca23d4
ingvar75 <ingvar0208@gmail.com>
2021-12-01 13:29:01 +0200

    l28chat add

--- l28-chat/app/Console/Components/WebSocketComponent.php
+++ l28-chat/app/Console/Components/WebSocketComponent.php
@@ -8,7 +8,14 @@ use Ratchet\MessageComponentInterface;
 
 class WebSocketComponent implements MessageComponentInterface
 {
+    private const TYPE_MESSAGE = 'message';
+    private const TYPE_SUBSCRIBE = 'subscribe';
+
+    /**
+     * @var ConnectionInterface[]
+     */
     private array $connections = [];
+    private array $subscriptions = [];
 
     function onOpen(ConnectionInterface $conn)
     {
@@ -29,8 +36,20 @@ class WebSocketComponent implements MessageComponentInterface
 
     function onMessage(ConnectionInterface $from, $msg)
     {
-        $message = json_decode($msg, true);
-        $data = $message['data'];
+        $payload = json_decode($msg, true);
+        switch ($payload['type']) {
+            case self::TYPE_MESSAGE:
+                $this->processMessage($payload);
+                break;
+            case self::TYPE_SUBSCRIBE:
+                $this->processSubscription($payload, $from);
+                break;
+        }
+    }
+
+    private function processMessage(array $payload): void
+    {
+        $data = $payload['data'];
 
         $message = new Message();
         $message->user_id = $data['userId'];
@@ -38,5 +57,38 @@ class WebSocketComponent implements MessageComponentInterface
         $message->text = $data['text'];
         $message->created_at = date('Y-m-d H:i:s', $data['time']);
         $message->save();
+
+        foreach ($this->subscriptions[$message->room_id] as $connectionId) {
+            $connection = $this->connections[$connectionId] ?? null;
+            if ($connection === null) {
+                continue;
+            }
+
+            $connection->send($message);
+        }
+    }
+
+    private function processSubscription(array $payload, ConnectionInterface $conn): void
+    {
+        $data = $payload['data'];
+
+        $roomId = $data['roomId'];
+        $this->subscribe($roomId, $conn->resourceId);
+    }
+
+    private function subscribe(int $roomId, int $connectionId): void
+    {
+        $this->clearSubscriptions($connectionId);
+        $this->subscriptions[$roomId][] = $connectionId;
+    }
+
+    private function clearSubscriptions(int $connectionId): void
+    {
+        foreach ($this->subscriptions as &$connections) {
+            $index = array_search($connectionId, $connections);
+            if ($index !== false) {
+                unset($connections[$index]);
+            }
+        }
     }
 }
